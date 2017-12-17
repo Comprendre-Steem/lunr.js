64691aca6e4fe81249378b442ffc7895c4a77e67
Olivier Roger <olivier.roger@gmail.com>
2017-12-17 08:05:37 +0100

    First working code with router

--- public/js/app.js
+++ public/js/app.js
@@ -30,57 +30,37 @@ var lunr_data = (function() {
     return posts;
 })();
 
-// Create global store
-// https://vuejs.org/v2/guide/state-management.html
-var store = {
-  debug: true,
-  state: { 
-    results: []
-  },
-  setSearchResults (newValue) {
-    if (this.debug) console.log('setMessageAction triggered with', newValue)
-    this.state.results = newValue
-  },
-  clearSearchResults () {
-    if (this.debug) console.log('clearMessageAction triggered')
-    this.state.results = null
-  }
-}
-
+// ROUTER 
+ 
+const Home = { template: '#page-home' }; 
+const Search = { template: '#page-search' }; 
+ 
+const routes = [ 
+  { path: '/', name: 'home', component: Home }, 
+  { path: '/search', name: 'search', component: Search } 
+]; 
+ 
+const router = new VueRouter({ 
+  routes 
+}); 
 
 // Define behavior for search results
-Vue.component('search-results', {
+
+let searchResult = Vue.component('search-result', {
   props: ['result'],
   template: '#result-template',
-  data: {
-    state: store.state
-  },
   computed: {
-    postUrl: function() {
+    postUrl() {
       return this.authorUrl + '/' + this.result.permlink;
     },
-    authorUrl: function() {
+    authorUrl() {
       return 'http://www.steemit.com/' + '@' + this.result.author;
     },
-    coverUrl: function() {
+    coverUrl() {
       return 'https://steemitimages.com/60x60/' + this.result.cover_image_url;
     }
   }
-});
-
-// ROUTER
-
-const Home = { template: '#page-home' };
-const Search = { template: '#page-search' };
-
-const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/search', name: 'search', component: Search }
-];
-
-const router = new VueRouter({
-  routes
-});
+})
 
 // Initialize the Vue Model
 var app = new Vue({
@@ -90,10 +70,11 @@ var app = new Vue({
     keywords: '',
     index: lunr_idx,
     posts: lunr_data,
-    state: store.state
+    results: []
   },
+  components: { searchResult },
   methods: {
-    search: function() {
+    search() {
       console.log("Searching for " + this.keywords);
       hits = this.index.search(this.keywords);
       // Extracts referencs (IDs)
@@ -102,10 +83,8 @@ var app = new Vue({
           refs.push(hits[o].ref);
       }
       // Find corresponding posts
-      this.store.state.results = this.posts.filter(entry => refs.includes(entry.ID));
+      this.results = this.posts.filter(entry => refs.includes(entry.ID));
+      this.$router.push({name: 'search', query: { q: this.keywords }})
     }
   }
 });
-
-// Always start from home
-router.push({ path: '/' });
