26252788bda44c812ee5182a61d0e4004a5dbf49
Olivier Roger <olivier.roger@gmail.com>
2017-12-17 04:23:54 +0100

    unsuccessful tests

--- public/js/App.js
+++ public/js/App.js
@@ -30,10 +30,31 @@ var lunr_data = (function() {
     return posts;
 })();
 
+// Create global store
+// https://vuejs.org/v2/guide/state-management.html
+var store = {
+  debug: true,
+  state: { 
+    results: []
+  },
+  setSearchResults (newValue) {
+    if (this.debug) console.log('setMessageAction triggered with', newValue)
+    this.state.results = newValue
+  },
+  clearSearchResults () {
+    if (this.debug) console.log('clearMessageAction triggered')
+    this.state.results = null
+  }
+}
+
+
 // Define behavior for search results
 Vue.component('search-results', {
   props: ['result'],
   template: '#result-template',
+  data: {
+    state: store.state
+  },
   computed: {
     postUrl: function() {
       return this.authorUrl + '/' + this.result.permlink;
@@ -45,30 +66,46 @@ Vue.component('search-results', {
       return 'https://steemitimages.com/60x60/' + this.result.cover_image_url;
     }
   }
-})
+});
+
+// ROUTER
+
+const Home = { template: '#page-home' };
+const Search = { template: '#page-search' };
+
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/search', name: 'search', component: Search }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 // Initialize the Vue Model
-var vm = new Vue({
-  el: '#vm',
+var app = new Vue({
+  router,
+  el: '#app',
   data: {
     keywords: '',
     index: lunr_idx,
     posts: lunr_data,
-    results: []
+    state: store.state
   },
   methods: {
     search: function() {
-      if (this.keywords != '') {
-        console.log("Searching for " + this.keywords);
-        hits = this.index.search(this.keywords);
-        // Extracts referencs (IDs)
-        var refs = new Array;
-        for(var o in hits) {
-            refs.push(hits[o].ref);
-        }
-        // Find corresponding posts
-        this.results = this.posts.filter(entry => refs.includes(entry.ID));
+      console.log("Searching for " + this.keywords);
+      hits = this.index.search(this.keywords);
+      // Extracts referencs (IDs)
+      var refs = new Array;
+      for(var o in hits) {
+          refs.push(hits[o].ref);
       }
+      // Find corresponding posts
+      this.store.state.results = this.posts.filter(entry => refs.includes(entry.ID));
     }
   }
 });
+
+// Always start from home
+router.push({ path: '/' });
